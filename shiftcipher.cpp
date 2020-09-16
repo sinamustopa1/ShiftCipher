ee089ff800c4f3f698d7c34fd55efe60f3ddae6b
sinamustopa <mustopa.sina@gmail.com>
2020-09-16 11:56:37 +0700

    Codingan

---
+++ shiftcipher.cpp
@@ -0,0 +1,62 @@
+#include <cstdlib>
+#include <iostream>
+#include <string.h>
+#define maks 500
+ 
+using namespace std;
+class Enkripsi{
+public:
+Enkripsi();
+void enkripsi();
+void deskripsi();
+void output();
+private:
+char chiper[maks];
+int key;
+char plain[maks];
+ 
+};
+ 
+Enkripsi::Enkripsi(){
+cout<<"Input kata plaint text : ";
+cin.getline(chiper,sizeof(chiper));
+cout<<"Masukkan key shift chiper  : ";
+cin>>key;
+cout<<endl;
+}
+ 
+void Enkripsi::enkripsi(){
+for(int i=0;i<strlen(chiper);i+=1){
+cout<<chiper[i]<<"=("<<int(chiper[i])<<") ";
+chiper[i] = (chiper[i]+key)%128;
+}
+}
+ 
+void Enkripsi::deskripsi(){
+for(int i=0;i<strlen(chiper);i+=1){
+plain[i] = (chiper[i]-key)%128;
+chiper[i] = plain[i];
+}
+}
+ 
+void Enkripsi::output(){
+for(int i=0;i<strlen(chiper);i+=1){
+cout<<chiper[i];
+}
+}
+ 
+int main(int argc, char *argv[])
+{
+Enkripsi Deskripsi;
+Deskripsi.enkripsi();
+cout<<"\n\nSetelah di Enkripsi : ";
+Deskripsi.output();
+Deskripsi.deskripsi();
+cout<<"\n\nKembali di Dekripsi : ";
+Deskripsi.output();
+ 
+cout<<endl<<endl;
+system("PAUSE");
+return EXIT_SUCCESS;
+}
+
